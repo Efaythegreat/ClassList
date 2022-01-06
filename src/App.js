7be86c36446abcfe865d5804cd0b617d0b1859a7
Efay <65887283+Efaythegreat@users.noreply.github.com>
2022-01-05 17:53:04 -0800

    Objected the thing Changed from 2D Arrays to using Objects, added new stuff(unfinished)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./styles.css";
 
 export default function App() {
+  /*
   let elements = [
     ["Victor", "Lymar", "He/Him"],
     ["Traci", "Siebel", "She/Her"],
@@ -29,13 +30,125 @@ export default function App() {
     });
     return pArray;
   }
+*/
+
+  let objects = [
+    {
+      f: "Victor",
+      l: "Lymar",
+      pronoun: "He/Him",
+      school: "LaunchDarkly",
+      graduationYear: 1923,
+      favoriteColor: "green"
+    },
+    {
+      f: "Traci",
+      l: "Siebel",
+      pronoun: "She/Her",
+      school: "LaunchDarkly",
+      graduationYear: 2022,
+      favoriteColor: "purple"
+    },
+    {
+      f: "Ida",
+      l: "Voong",
+      pronoun: "She/Her",
+      school: "Oakland Technical High School",
+      graduationYear: 2022,
+      favoriteColor: "blue"
+    },
+    {
+      f: "Ana",
+      l: "Rivera",
+      pronoun: "She/Her",
+      school: "CSU East Bay",
+      graduationYear: 0,
+      favoriteColor: ""
+    },
+    {
+      f: "Aidan",
+      l: "Fuller",
+      pronoun: "He/Him",
+      school: "",
+      graduationYear: 0,
+      favoriteColor: ""
+    },
+    {
+      f: "kyle",
+      l: "wan",
+      pronoun: "He/Him",
+      school: "",
+      graduationYear: 0,
+      favoriteColor: ""
+    },
+    {
+      f: "Enjun",
+      l: "Li",
+      pronoun: "He/Him",
+      school: "",
+      graduationYear: 0,
+      favoriteColor: ""
+    },
+    {
+      f: "Luis",
+      l: "Garcia",
+      pronoun: "He/Him",
+      school: "CSU East Bay",
+      graduationYear: 0,
+      favoriteColor: ""
+    },
+    {
+      f: "Amanda",
+      l: "Yu",
+      pronoun: "She/Her",
+      school: "Oakland Technical High School",
+      graduationYear: 0,
+      favoriteColor: ""
+    },
+    {
+      f: "Yifei",
+      l: "Du",
+      pronoun: "He/Him",
+      school: "",
+      graduationYear: 0,
+      favoriteColor: ""
+    },
+    {
+      f: "Hector ",
+      l: "Del Valle",
+      pronoun: "He/Him",
+      school: "",
+      graduationYear: 0,
+      favoriteColor: ""
+    },
+    {
+      f: "HaoYu",
+      l: "Lin",
+      pronoun: "He/Him",
+      school: "Oakland Technical High School",
+      graduationYear: 0,
+      favoriteColor: ""
+    }
+  ];
 
   return (
     <div className="App">
       <h1>Hello Team</h1>
       <h2>Start editing to see some magic happen!</h2>
-
-      {makeDiv2(elements)}
+      {objects.map(function (obj) {
+        return (
+          <div>
+            <div>
+              Name: {obj.f} {obj.l}
+            </div>
+            <div>Pronoun: {obj.pronoun}</div>
+            <div>School: {obj.school}</div>
+            <div>Graduation Year: {obj.guaduationYear}</div>
+            <div>Favorite Color: {obj.favoriteColor}</div>
+            <p></p>
+          </div>
+        );
+      })}
     </div>
   );
 }
